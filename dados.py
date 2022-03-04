fc148f75daafb051b172beba680bbf2c90e23ae1
Samira-Kaline <36956493+Samira-Kaline@users.noreply.github.com>
2022-03-04 11:12:49 -0300

    Adicionando a lista de dados e o main.html

---
+++ dados.py
@@ -0,0 +1,5 @@
+clientes = [
+   { "nome": "Samira", "idade": 19,"telefone": "0000-1111" },
+   { "nome": "Lilia", "idade": 20,"telefone": "1234-4321"  },
+   { "nome": "Maria", "idade": 15,"telefone": "9876-6789"  }
+ ]
